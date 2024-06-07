9d3b856e89fd5b378705f660f00bfff7cb7eed5e
Norman Breau <norman@breautek.com>
2024-06-07 12:21:26 -0300

    makeRelease script

---
+++ makeRelease.sh
@@ -0,0 +1,32 @@
+
+source build-tools/assertions.sh
+
+assertGitRepo
+assertCleanRepo
+
+VERSION="$1"
+
+assertVersion $VERSION
+assertGitTagAvailable v$VERSION
+
+npm test
+assertLastCall
+
+npm run build:debug
+assertLastCall
+
+npm version $VERSION-debug --no-commit-hooks --no-git-tag-version
+assertLastCall
+
+npm publish --tag debug
+assertLastCall
+
+pm run build
+npm version $VERSION
+npm publish
+
+git add package.json package-lock.json
+git commit -m "Release: $VERSION"
+git tag -a v$VERSION -m "Release: $VERSION"
+git push
+git push --tags
